a1affe8289c943592b3e064d7daab96297235fa1
Julio Cesar <juliocesarmartinez1344@gmail.com>
2023-11-12 21:19:00 -0500

    xd

--- controllers/productos/controller.js
+++ controllers/productos/controller.js
@@ -47,7 +47,7 @@ export const getProductosFiltrados = async (
         query = `
         SELECT * FROM productos 
         WHERE ((precio_exito > 0 AND precio_exito < ?) OR (precio_olim > 0 AND precio_olim < ?) OR (precio_d1 > 0 AND precio_d1 < ?)) AND categoria_id = ?`;
-        values.push(precioMaximo, precioMaximo); // Agregar dos veces más para los otros distribuidores
+        values = [precioMaximo, precioMaximo, precioMaximo, categoriaid];
     }
     console.log({ query, values });
 
