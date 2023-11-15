e5897aaacbd498c9d718351266b3a4e9619cc9c1
Julio Cesar <juliocesarmartinez1344@gmail.com>
2023-11-15 05:38:23 -0500

    update db

--- db/db.js
+++ db/db.js
@@ -3,10 +3,10 @@ import mysql from "mysql2";
 export const PoolDB = () => {
   const pool = mysql
     .createPool({
-      host: "sql10.freemysqlhosting.net",
-      user: "sql10660099",
-      password: "VIYSGyKZFP",
-      database: "sql10660099",
+      host: "sql10.freesqldatabase.com",
+      user: "sql10662344",
+      password: "dJx83BFDcI",
+      database: "sql10662344",
     })
     .promise();
   return pool;
