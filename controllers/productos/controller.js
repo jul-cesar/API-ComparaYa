980e195212eae6f1f2514dd7200f858aafab8b18
Julio Cesar <juliocesarmartinez1344@gmail.com>
2023-12-20 14:39:30 -0500

    url

--- controllers/productos/controller.js
+++ controllers/productos/controller.js
@@ -108,3 +108,5 @@ export const SumPrice = async () => {
   );
   return precioTotal.rows[0];
 };
+
+console.log(await getProductos());
--- db/db.js
+++ db/db.js
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 const { Pool } = pkg;
 
-// Rest of your code using Pool
-
 export const PoolDB = () => {
   const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    connectionString:
+      "postgres://default:O7iwJVAbKt0r@ep-damp-recipe-48184732-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" +
+      "?sslmode=require",
   });
 
   return pool;
