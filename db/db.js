d53dc9763cc9ccc01d812b3851dcf89f72e3436e
Julio Cesar <juliocesarmartinez1344@gmail.com>
2024-01-04 16:20:26 -0500

    env

--- db/db.js
+++ db/db.js
@@ -5,9 +5,7 @@ const { Pool } = pkg;
 
 export const PoolDB = () => {
   const pool = new Pool({
-    connectionString:
-      "postgres://default:W3Ui9sSnbkwH@ep-billowing-glade-39781605-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" +
-      "?sslmode=require",
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   });
 
   return pool;
